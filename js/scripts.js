5419bf56408632a71f677183e83418b809ae2594
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-11-11 16:07:04 +0100

    feat: countdown timer added

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,37 @@
 // Dati
 const timerElem = document.querySelector(".timer");
+const descriptionElem = document.querySelector(".description");
 const rngFieldElem = document.querySelector(".rng-field");
 const randomNumElem = document.querySelectorAll(".random-number");
 const inputFieldElem = document.querySelector(".input-field");
 const allInputs = document.querySelectorAll("input");
+const btnFieldElem = document.querySelector(".btn-section");
 const btnElem = document.querySelector(".btn");
 const randomNumbers = [];
 
+// Numero Random
 for (let i = 0; i < randomNumElem.length; i++) {
-    const curNum = randomNum(1, 100);
+    const curNum = randomNum(1, 99);
     randomNumbers.push(curNum);
     randomNumElem[i].innerHTML = curNum;
 }
 
+// Countdown
+let counter = 5;
+
+const intervalId = setInterval(function() {
+	timerElem.innerHTML = counter;
+	counter--;
+
+	if(counter < 0) {
+		rngFieldElem.classList.add("d-none")
+        inputFieldElem.classList.remove("d-none");
+        btnFieldElem.classList.remove("d-none");
+		timerElem.innerHTML = "Scegli i tuoi numeri!";
+		clearInterval(intervalId);
+	}
+	
+}, 1000);
 
 ///////////////
 // FUNZIONI //
