d8520111766fe5f99ff8477fe2aca560b6408cd4
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-11-11 17:25:26 +0100

    feat: button functionality added

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,10 @@ const descriptionElem = document.querySelector(".description");
 const rngFieldElem = document.querySelector(".rng-field");
 const randomNumElem = document.querySelectorAll(".random-number");
 const inputFieldElem = document.querySelector(".input-field");
-const allInputs = document.querySelectorAll("input");
+const allInputs = document.querySelectorAll(".input-field input");
 const btnFieldElem = document.querySelector(".btn-section");
 const btnElem = document.querySelector(".btn");
+const resultElem = document.querySelector(".result")
 const randomNumbers = [];
 
 // Numero Random
@@ -30,8 +31,35 @@ const intervalId = setInterval(function() {
 		timerElem.innerHTML = "Scegli i tuoi numeri!";
 		clearInterval(intervalId);
 	}
-	
 }, 1000);
+
+// Bottone
+const userNum = [];
+
+btnElem.addEventListener("click", function() {
+    userNum.length = 0;
+    
+    for (let i = 0; i < allInputs.length; i++) {
+        userNum.push(Number(allInputs[i].value));
+    }
+
+    const correctNumbers = userNum.filter(function(sum) {
+       return randomNumbers.includes(sum);
+    });
+
+    if (correctNumbers.length === 5) {
+        timerElem.innerHTML= `Sei un campione!`
+        resultElem.innerHTML = `Hai indovinato tutti i numeri! (${correctNumbers.join(", ")})`;
+    } else if (correctNumbers.length === 1) {
+        resultElem.innerHTML = `Hai indovinato ${correctNumbers.length} numero! (${correctNumbers.join(", ")})`;
+    } else if (correctNumbers.length === 0) {
+        timerElem.innerHTML= `Che peccato!`
+        resultElem.innerHTML = `Hai indovinato ${correctNumbers.length} numeri! Riprova!`;
+    } else {
+        resultElem.innerHTML = `Hai indovinato ${correctNumbers.length} numeri! (${correctNumbers.join(", ")})`;
+    }
+
+});
 
 ///////////////
 // FUNZIONI //
@@ -42,3 +70,4 @@ function randomNum(min, max) {
     const randomInt = Math.floor(Math.random() * (max - min + 1) + min);
     return randomInt;
 }
+
